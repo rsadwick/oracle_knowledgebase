9ad38f53aca272061d98918518105f2415d61b6e
Ryan Sadwick <rsadwick@gmail.com>
2014-04-25 14:32:18 -0400

    added RightNow client demo
    uses proxy class

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hsn.RightNow.Proxy;
+
+namespace ConsoleApplication1
+{
+    class Program {
+        private RightNowKnowledgePortClient _client;
+
+        public SampleKFClient() {
+            _client = new RightNowKnowledgePortClient();
+        }
+
+        static void Main(string[] args)
+        {
+            SampleKFClient sampleClient = new SampleKFClient();
+        }
+    }
+}
